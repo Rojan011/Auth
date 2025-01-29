ac598506acfe6d8b010b22ce759f187a4d2ecedd
Rojan <shrestharojan79@gmail.com>
2025-01-29 13:36:24 +0545

    feature-changePassword

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -99,4 +99,52 @@ const loginUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser, loginUser };
+//change password
+const changePassword = async (req, res) => {
+  try {
+    //1. First of all we get the userId
+    const userId = req.userInfo.userId;
+
+    //2. Then we need to extract the old password as well as the new password so
+    const { oldPassword, newPassword } = req.body;
+
+    //3. Then we need to find out about the current user
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "The user is not found",
+      });
+    }
+
+    //4. Then we need to check if the extracted password matches with the password of the user that we are going to be working with
+    const isPasswordMatched = await bcrypt.compare(oldPassword, user.password);
+    if (!isPasswordMatched) {
+      return res.status(400).json({
+        success: false,
+        message: "The password you have entered is incorrect!",
+      });
+    }
+
+    //5. Now after the user enters the new password then we need to store that password only after hashig it as done before
+    const salt = await bcrypt.genSalt(10);
+    const newHashedPassword = await bcrypt.hash(newPassword, salt);
+
+    //update user password
+    user.password = newHashedPassword;
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Password Changed Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured please try again",
+    });
+  }
+};
+
+module.exports = { registerUser, loginUser, changePassword };
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser } = require("../controllers/auth-controller");
+const {
+  registerUser,
+  loginUser,
+  changePassword,
+} = require("../controllers/auth-controller");
+
+//We also extract authmiddleware inorder to ensure the privacy whilst changing the password
+const authMiddleware = require("../middleware/auth-middleware");
 
 //all routes are related to authentication & authorization
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/change-password", authMiddleware,changePassword);
 
 module.exports = router;
